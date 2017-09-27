4abeeb1cef4dffddf1f825e9fcd97db4fa3a5c14
Ebisuda <mebisuda@jbs.com>
2017-09-28 00:36:12 +0900

    added codes for provisioning member servers.

--- S2DClusterDemo.ps1
+++ S2DClusterDemo.ps1
@@ -6,19 +6,37 @@ $deploymentName = "S2DDemo"
 $resourceGroupName = "mebisuda-S2DDemo"
 $location = "Japan East"
 
+#deploy parameters for new AD forest
 $adminUsername = "mebisuda"
+$adminPassword = Read-Host -AsSecureString -Prompt "password"
 $domainName = "s2d.test"
-$dnsPrefix = ("mebisudas2ddemo" + $randomNumber)
+$dnsPrefix = ("mebisudas2ddemoadvm")
+
+#deploy parameters for member servers
+$existingVNETName = "adVNET"
+$existingSubnetName = "adSubnet"
+$dnsLabelPrefix = "mebis2dnode"
+$vmSize = "Standard_D2"
+$ouPath = 'CN=Computers;DC=s2d;DC=test'
+
 
 Get-AzureRmSubscription -SubscriptionName $subscriptionName | Select-AzureRmSubscription
 
 # create resource group
 New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
 
-# create new ad forest
+# create new ad forest(adVM)
 # https://azure.microsoft.com/ja-jp/resources/templates/active-directory-new-domain/
-New-AzureRmResourceGroupDeployment -DeploymentDebugLogLevel All -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateUri https://raw.githubusercontent.com/azure/azure-quickstart-templates/master/active-directory-new-domain/azuredeploy.json `
--adminUsername $adminUsername -domainName $domainName -dnsPrefix $dnsPrefix 
+New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateUri https://raw.githubusercontent.com/azure/azure-quickstart-templates/master/active-directory-new-domain/azuredeploy.json `
+-adminUsername $adminUsername -adminPassword $adminPassword -domainName $domainName -dnsPrefix $dnsPrefix 
 
+# add new server to the new ad domain
+# https://azure.microsoft.com/ja-jp/resources/templates/201-vm-domain-join/
+New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateUri https://raw.githubusercontent.com/azure/azure-quickstart-templates/master/201-vm-domain-join/azuredeploy.json `
+-existingVNETName $existingVNETName -existingSubnetName $existingSubnetName -dnsLabelPrefix ($dnsLabelPrefix+"1") -vmSize $vmSize -domainToJoin $domainName -domainUsername $adminUserName -domainPassword $adminPassword -ouPath $ouPath -vmAdminUsername $adminUsername -vmAdminPassword $adminPassword
 
+New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateUri https://raw.githubusercontent.com/azure/azure-quickstart-templates/master/201-vm-domain-join/azuredeploy.json `
+-existingVNETName $existingVNETName -existingSubnetName $existingSubnetName -dnsLabelPrefix ($dnsLabelPrefix+"2") -vmSize $vmSize -domainToJoin $domainName -domainUsername $adminUserName -domainPassword $adminPassword -ouPath $ouPath -vmAdminUsername $adminUsername -vmAdminPassword $adminPassword
 
+New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateUri https://raw.githubusercontent.com/azure/azure-quickstart-templates/master/201-vm-domain-join/azuredeploy.json `
+-existingVNETName $existingVNETName -existingSubnetName $existingSubnetName -dnsLabelPrefix ($dnsLabelPrefix+"3") -vmSize $vmSize -domainToJoin $domainName -domainUsername $adminUserName -domainPassword $adminPassword -ouPath $ouPath -vmAdminUsername $adminUsername -vmAdminPassword $adminPassword
