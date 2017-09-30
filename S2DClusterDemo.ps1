0a8686dab653f31f2b12dc4037b2e843c5af6b0c
Ebisuda <mebisuda@jbs.com>
2017-09-30 10:34:09 +0900

    added all disk clean up logic.

--- S2DClusterDemo.ps1
+++ S2DClusterDemo.ps1
@@ -75,3 +75,22 @@ Enable-PSRemoting
 Set-Item WSMan:\localhost\Client\TrustedHosts "*"
 Invoke-Command -ComputerName mebis2dnode1, mebis2dnode2, mebis2dnode3, mebis2dnode4 -Credential Get-Credential -ScriptBlock {Add-WindowsFeature Failover-Clustering -IncludeManagementTools}
 
+
+#clean up all disks
+Invoke-Command (Get-Cluster -Name s2dcluster -Domain s2d.test| Get-ClusterNode) -Credential (Get-Credential) {
+    Update-StorageProviderCache
+    Get-StoragePool | Where-Object IsPrimordial -eq $false | Set-StoragePool -IsReadOnly:$false -ErrorAction SilentlyContinue
+    Get-StoragePool | Where-Object IsPrimordial -eq $false | Get-VirtualDisk | Remove-VirtualDisk -Confirm:$false -ErrorAction SilentlyContinue
+    Get-StoragePool | Where-Object IsPrimordial -eq $false | Remove-StoragePool -Confirm:$false -ErrorAction SilentlyContinue
+    Get-PhysicalDisk | Reset-PhysicalDisk -ErrorAction SilentlyContinue
+    Get-Disk | Where-Object Number -ne $null | Where-Object IsBoot -ne $true | Where-Object IsSystem -ne $true | Where-Object PartitionStyle -ne RAW | ForEach-Object {
+    $_ | Set-Disk -isoffline:$false
+    $_ | Set-Disk -isreadonly:$false
+    $_ | Clear-Disk -RemoveData -RemoveOEM -Confirm:$false
+    $_ | Set-Disk -isreadonly:$true
+    $_ | Set-Disk -isoffline:$true
+    }
+    
+    Get-Disk |Where-Object Number -ne $null |Where-Object IsBoot -ne $true |Where-Object IsSystem -ne $true |Where-Object PartitionStyle -eq RAW | Group-Object -NoElement -Property FriendlyName
+    
+    } | Sort-Object -Property PsComputerName,Count
