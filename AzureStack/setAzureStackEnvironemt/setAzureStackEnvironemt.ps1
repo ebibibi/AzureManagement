a7a2c739951b520cff049edcca73dc4a3d5fed34
Masahiko Ebisuda <mebisuda@azurestack.local>
2017-11-16 23:42:46 +0900

    create new storage account demo ps1 file.

--- AzureStack/setAzureStackEnvironemt/setAzureStackEnvironemt.ps1
+++ AzureStack/setAzureStackEnvironemt/setAzureStackEnvironemt.ps1
@@ -66,42 +66,6 @@ Get-AzureRmEnvironment -Name AzureStackAdmin
 Get-AzureRmEnvironment -Name AzureStackUser
 
 
-#----------------------------------
-#Create Storage Account on Azure 
-#----------------------------------
-Login-AzureRmAccount -EnvironmentName "AzureCloud"           
-
-#Select Azure Subscription
-Select-AzureRmSubscription -SubscriptionName "PublicAzure"
-
-#Create Resource Group
-New-AzureRmResourceGroup -Name PublicAzureRG -Location "japan east"
-
-#Create Storage Account
-New-AzureRmResourceGroupDeployment -Name publicazurestorage -ResourceGroupName PublicAzureRG `
-  -TemplateFile D:\work\AzureStack-QuickStart-Templates-master\AzureStack-QuickStart-Templates-master\101-create-storage-account\azuredeploy.json -storageAccountType Standard_LRS
-
-#----------------------------------
-#Check your portal 
-#----------------------------------
-
-#----------------------------------
-#Create Storage Account on Azure Stack
-#----------------------------------
-Login-AzureRmAccount `
-  -EnvironmentName "AzureStackUser" `
-  -TenantId $TenantIDforUser
-
-
-#Select Azure Subscription
-Select-AzureRmSubscription -SubscriptionName "AzureStack"
-
-#Create Resource Group
-New-AzureRmResourceGroup -Name AzureStackRG -Location "local"
-
-#Create Storage Account
-New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName AzureStackRG `
-  -TemplateFile D:\work\AzureStack-QuickStart-Templates-master\AzureStack-QuickStart-Templates-master\101-create-storage-account\azuredeploy.json -storageAccountType Standard_LRS
 
 #--------------------------------------------------------------------
 #Create a AD domain controller server non-HA with PowerShell DSC Extension
