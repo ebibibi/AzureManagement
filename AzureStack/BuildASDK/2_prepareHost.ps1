fa87f672d646f537b52477d218732ba003fea096
Masahiko Ebisuda <ebibibi@gmail.com>
2018-04-15 15:21:57 +0900

    add preparehost script

---
+++ AzureStack/BuildASDK/2_prepareHost.ps1
@@ -0,0 +1,13 @@
+# Variables
+$Uri = 'https://raw.githubusercontent.com/Azure/AzureStack-Tools/master/Deployment/asdk-installer.ps1'
+$LocalPath = 'C:\AzureStack_Installer'
+# Create folder
+New-Item $LocalPath -Type directory
+# Enforce usage of TLSv1.2 to download from GitHub
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# Download file
+Invoke-WebRequest $uri -OutFile ($LocalPath + '\' + 'asdk-installer.ps1')
+
+# StartScript
+cd $LocalPath
+.\asdk-installer.ps1
