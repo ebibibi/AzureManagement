59ac68b9006c1adf9e619a98a336c3c98157812a
ibeonlyone <35884549+ibeonlyone@users.noreply.github.com>
2018-06-20 18:38:00 +0900

    CHANGE IP

--- AzureStack/BuildASDK/3_deployASDK.ps1
+++ AzureStack/BuildASDK/3_deployASDK.ps1
@@ -1,3 +1,3 @@
 $adminpass = Read-Host -password -AsSecureString
 Set-Location c:\CloudDeployment\Setup
-.\installAzureStackPOC.ps1 -AdminPassword $adminpass -InfraAzureDirectoryTenantName jbsazsta.onmicrosoft.com -NatIPv4Subnet 172.29.103.0/24 -NatIPv4Address 172.29.103.1 -NatIPv4DefaultGateway 172.29.103.250 -TimeServer 210.173.160.27
+.\installAzureStackPOC.ps1 -AdminPassword $adminpass -InfraAzureDirectoryTenantName jbsazsta.onmicrosoft.com -NatIPv4Subnet 172.29.103.0/24 -NatIPv4Address 172.29.103.2 -NatIPv4DefaultGateway 172.29.103.250 -TimeServer 210.173.160.27
