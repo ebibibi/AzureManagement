11d9b16f00cc09b36226b6797c051859d4e6daea
ibeonlyone <35884549+ibeonlyone@users.noreply.github.com>
2018-06-21 15:32:56 +0900

    import Azure Stack Module first

--- AzureStack/BuildASDK/5_RegisterToAzure.ps1
+++ AzureStack/BuildASDK/5_RegisterToAzure.ps1
@@ -1,4 +1,8 @@
-﻿
+﻿Install-Module -Name AzureRm.BootStrapper -Force
+Use-AzureRmProfile -Profile 2017-03-09-profile
+Install-Module -Name AzureStack -RequiredVersion 1.2.11 -Forc
+
+
 # Add the Azure cloud subscription environment name. Supported environment names are AzureCloud or, if using a China Azure Subscription, AzureChinaCloud.
 Login-AzureRmAccount -EnvironmentName "AzureCloud"
 
