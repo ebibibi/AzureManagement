7fbbc7675c212274122cf763f2afdbbfffc604e7
Masahiko Ebisuda <ebibibi@gmail.com>
2018-06-28 00:01:51 +0900

    changed resorce names.

--- Terraform/CreateGitLabCommunityEdition_VM/creategitlab.tf
+++ Terraform/CreateGitLabCommunityEdition_VM/creategitlab.tf
@@ -18,7 +18,7 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
 
 # Create subnet
 resource "azurerm_subnet" "myterraformsubnet" {
-  name                 = "Subnet"
+  name                 = "Subnet1"
   resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
   virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
   address_prefix       = "10.0.1.0/24"
