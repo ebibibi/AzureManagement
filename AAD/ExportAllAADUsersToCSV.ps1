e34b790d9596ef9b57368eee652e268ef9a0a6cd
Masahiko Ebisuda <ebibibi@gmail.com>
2018-09-03 22:03:46 +0900

    add some attributes to csv format.

--- AAD/ExportAllAADUsersToCSV.ps1
+++ AAD/ExportAllAADUsersToCSV.ps1
@@ -1,5 +1,5 @@
 Connect-AzureAD
 $users = Get-AzureADUser -All $true
-$users | Select-Object Mail, Surname | Export-csv -NoTypeInformation -Encoding UTF8 c:\tmp\aadusers.csv
+$users | Select-Object Mail, Surname, Department, JobTitle | Export-csv -NoTypeInformation -Encoding UTF8 c:\tmp\aadusers.csv
 
 
