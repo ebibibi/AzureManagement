c6027c73dda8deb004d73a53d646ac399579eff4
Masahiko Ebisuda <ebibibi@gmail.com>
2018-10-09 20:44:52 +0900

    add ListAADApplicationsAndOwner.ps1

---
+++ AAD/ListAADApplicationsAndOwner.ps1
@@ -0,0 +1,11 @@
+﻿install-module AzureAD | Out-Null
+Connect-AzureAD | Out-Null
+
+$applications = Get-AzureADApplication
+Write-Output "DiskplayName, ObjectId, AppId, Owner_DisplayName, Owner_UserPrincipalName, Owner_UserType"
+foreach($application in $applications) {
+    $info = ""
+    $owner = $application | Get-AzureADApplicationOwner
+    $info = ('"' + $application.DisplayName + '","' + $application.ObjectId + '","' + $application.AppId + '","' + $owner.DisplayName + '","' + $owner.UserPrincipalName + '","' + $owner.UserType + '"')
+    Write-Output $info
+}
